384e4513b0edfe0d3ff69f4c4344f6396338267a
gursam <gursam.madan@gmail.com>
2025-07-06 14:37:12 +0530

    Project complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import "./App.css";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-
+import Features from "./components/Features";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
       <About />
+      <Features />
+      <Contact />
+      <Footer />
     </main>
   );
 }
